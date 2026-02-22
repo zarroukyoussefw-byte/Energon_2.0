4414107df636d2203c4688c92f41573becf0e67f
zarroukyoussefw-byte <zarroukyoussef.w@gmail.com>
2026-02-22 18:27:09 +0100

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  output: 'static',
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'auto',
+  },
+  vite: {
+    build: {
+      cssMinify: true,
+    },
+  },
+});
